7350f006731fb53e5e62a425f59c13816ad0560b
AndreyZelikay <49722390+AndreyZelikay@users.noreply.github.com>
2019-10-17 10:26:22 +0300

    Add files via upload

--- VMA_Lab1/src/main/SolveEquation.java
+++ VMA_Lab1/src/main/SolveEquation.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 public class SolveEquation {
 
     public static ArrayList<Double> byGauss(SystemOfEquations systemOfEquations){
-        ArrayList<Double> xVector = new ArrayList<>(Collections.nCopies(systemOfEquations.getSolutions().size(), (double) 0));
+        ArrayList<Double> xVector = new ArrayList<>(Collections.nCopies(systemOfEquations.getSolutions().size(), null));
         Matrix matrix = new Matrix();
         double determinant = 1;
         matrix.copyOf(systemOfEquations.getCoefficientMatrix());
@@ -16,14 +16,23 @@ public class SolveEquation {
         for(int j = 0; j < matrix.getSize(); j++){
             bufferMatrix.choiceHighElement();
             determinant *= bufferMatrix.getElements().get(0).get(0)*bufferMatrix.getDeterminant();
-            /*if(bufferMatrix.getElements().get(0).get(0)==0){
-                for(int i = 0;i < bufferMatrix.getSize();i++){
-                    if(bufferMatrix.getElements().get(i).get(bufferMatrix.getSize())!= 0){
-                        System.out.println("System has no solutions!");
+            if(bufferMatrix.getElements().get(0).get(0)==0){
+                if(bufferMatrix.getElements().get(0).get(bufferMatrix.getSize()-1) !=0 ) {
+                    boolean isPossibleToSolve = false;
+                    for (int i = 0; i < bufferMatrix.getElements().get(0).size(); i++) {
+                        if (bufferMatrix.getElements().get(0).get(i) != 0) {
+                            isPossibleToSolve = true;
+                            break;
+                        }
+                    }
+                    if(!isPossibleToSolve){
+                        System.out.println("Can not solve this system!!!");
                         System.exit(0);
                     }
                 }
-            }*/
+                xVector.set(j,(double) Math.round(((Math.random() * 20) -10)*100)/100);
+                System.out.println("x"+j+"was generate randomly!");
+            }
             bufferMatrix.shareStringByFirstElement(0);
             for(int k = 1; k < bufferMatrix.getSize();k++){
                 bufferMatrix.substractFirstLine(k);
@@ -33,6 +42,7 @@ public class SolveEquation {
         }
         systemOfEquations.getCoefficientMatrix().setDeterminant(determinant);
         for(int i = matrix.getSize() - 1; i >= 0; i--){
+            if(xVector.get(i)==null) {
             double sum = 0;
             for(int j = 0; j < matrix.getSize(); j++){
                 double element = matrix.getElements().get(i).get(j);
@@ -40,7 +50,8 @@ public class SolveEquation {
                     sum += element*xVector.get(j);
                 }
             }
-            xVector.set(i,(matrix.getElements().get(i).get(matrix.getElements().get(i).size()-1) - sum));
+                xVector.set(i, (matrix.getElements().get(i).get(matrix.getElements().get(i).size() - 1) - sum));
+            }
         }
         return xVector;
     }
